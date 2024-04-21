3343ed453cc4116182797bff487b926928a3c797
shahad-jawabreh <s12028521@stu.najah.edu>
2024-04-21 21:48:07 +0300

    Finishh

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import cors  from 'cors'
 import initapp from './app.router.js'
 const app = express()
+app.use(cors())
 initapp(app,express)
 const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>{
